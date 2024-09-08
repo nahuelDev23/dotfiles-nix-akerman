7b406ce67b5115dc23ded7a1943048d110ef7472
nahuelDev23 <nahuel.dev.23@gmail.com>
2024-09-08 09:08:48 -0300

    waybar css

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.grub.useOSProber = true;
 
   networking.hostName = "akerman"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
