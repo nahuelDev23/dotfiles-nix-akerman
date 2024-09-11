7bb9397dc6a7d2011213af148d79d874cdca0160
nahuelDev23 <nahuel.dev.23@gmail.com>
2024-09-11 13:49:49 -0300

    fon isevka nerd font

--- home-manager/fonts/default.nix
+++ home-manager/fonts/default.nix
@@ -1,11 +1,30 @@
 {
-pkgs,
-...
-}:{
-fonts.fontconfig.enable=true;
+  pkgs,
+  ...
+}: {
+  # Habilitar fontconfig
+  fonts.fontconfig.enable = true;
 
-home.packages = with pkgs; [
-victor-mono
+  # Incluir el paquete de Iosevka Nerd Font
+  home.packages = with pkgs; [
+    nerd-fonts-iosevka
+  ];
 
-];
+  # Configurar las fuentes para Iosevka Nerd Font
+  fonts = {
+    enable = true;
+
+    # Definir las fuentes adicionales
+    fontDirs = [
+      "${pkgs.nerd-fonts-iosevka}/share/fonts"
+    ];
+
+    # Especificar las fuentes preferidas para monospace y nerd fonts
+    fontconfig = {
+      settings = {
+        "monospace" = [ "Iosevka Nerd Font" ];
+      };
+    };
+  };
 }
+
